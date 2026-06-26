cc2c561a344a4b823487c662a61ea5b9bca60214
pyao12 <patrick25yao@outlook.com>
2026-06-27 07:53:27 +0800

    OpenAI Completions Request

--- src/aurona/main.py
+++ src/aurona/main.py
@@ -1,4 +1,56 @@
 from . import __version__
 
+import json
+import sys
+from aurona.provider import openai
+
+
 def main():
     print(f"Welcome to Aurona (version {__version__})")
+
+    try:
+        with open("data/providers.json", "r", encoding="utf-8") as f:
+            providers = json.loads(f.read())
+    except FileNotFoundError:
+        print("No providers found!")
+        return 1
+
+    try:
+        test_provider = providers[0]
+    except IndexError:
+        print("No providers found!")
+        return 1
+
+    # 太不优雅了 (
+    if (
+        not test_provider.get("name")
+        or not test_provider.get("display")
+        or not test_provider.get("api-key")
+        or not test_provider.get("baseurl")
+    ):
+        print("Providers didn't configured correctly!")
+        return 1
+
+    print(f"Test with {test_provider.get("display")}")
+    print("Creating client...")
+    connection = openai.OpenAIConnection(
+        test_provider.get("baseurl"), test_provider.get("api-key")
+    )
+
+    try:
+        QUESTION = sys.argv[1]
+    except IndexError:
+        print("Please give your question!")
+        return 1
+
+    print(f"Question: {QUESTION}")
+
+    # 作者用的Deepseek作为测试，当然只是测试啦，后面会有更优秀的数据存储
+    answer = connection.get_completions(
+        openai.build_temp_history(QUESTION), "deepseek-v4-flash"
+    )
+    print("=" * 20)
+    print(answer)
+    print("=" * 20)
+
+    return 0  # C++写多了哈哈
---
+++ src/aurona/provider/openai.py
@@ -0,0 +1,22 @@
+# OpenAI Compatible接口
+
+from openai import OpenAI
+
+
+# 因为completions请求必须传递整个history，所以有这么个函数构建临时的消息列表
+# responses请求就不用了
+def build_temp_history(message: str) -> list:
+    return [{"role": "user", "content": message}]
+
+
+class OpenAIConnection:
+    # OpenAI Compatible接口的连接实例，方便同时连接多个providers
+
+    def __init__(self, url: str, key: str):
+        self._openai_client = OpenAI(api_key=key, base_url=url)
+
+    def get_completions(self, messages_history: list, model: str) -> str:
+        completion = self._openai_client.chat.completions.create(
+            model=model, messages=messages_history
+        )
+        return completion.choices[0].message.content
